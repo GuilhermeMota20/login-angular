d7220f9c119ebba1a213bdf881b77885894bf6a2
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2022-06-21 17:39:50 -0300

    FT - creating validation with froms

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario = '';
+  senha = '';
 
-  ngOnInit(): void {
+  constructor(private authService: AutenticacaoService) { }
+
+  ngOnInit(): void { }
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha)
+      .subscribe(
+        (res) => {
+          console.log(res);
+        },
+        (err) => {
+          console.error(err)
+        }
+      )
   }
-
 }
